6cd0cc8d1619997faf8088eacefe5e4aa355a483
Anurada <anurada119@gmail.com>
2016-09-18 01:06:51 +0530

    added js with uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 gulp.task('jade', function() {
     return gulp.src('src/templates/**/*.jade')
@@ -11,5 +12,11 @@ gulp.task('jade', function() {
 gulp.task('sass', function() {
     return gulp.src('src/sass/main.sass')
         .pipe(sass())
-        .pipe(gulp.dest('builds/development/css'))
+        .pipe(gulp.dest('builds/developments/css'))
 })
+
+gulp.task('js', function() {
+    return gulp.src('src/js/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('builds/developments/js'))
+});
